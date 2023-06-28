aaf0e73b55e88c89730c69a462be72f8ca93fb12
J.M. Cléry <jmclery@gmail.com>
2023-06-28 14:56:31 +0200

    Ajout de la view liste

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
+import ListView from '@/views/ListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,9 +9,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: ListView
     }
   ]
 })
 
-export default router;
+export default router
